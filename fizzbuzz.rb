42bc1e8bfcf15eae36b5cc29c95c9b8bdf6ad0d7
Emily Jirles <emily.jirles@gmail.com>
2018-04-24 14:45:47 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(num)
-  if input%3 == 0 
-    if input%5 == 0 
+  if input % 3 == 0 
+    if input % 5 == 0 
       puts "FizzBuzz"
     end
     puts "Fizz"
   end
-  elsif input%5 == 0 
+  elsif input % 5 == 0 
     puts "Buzz" 
   end 
   else 
