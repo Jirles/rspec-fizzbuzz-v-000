1dd35501b072a1d1cd27450c7b5aa5549eb5d868
Emily Jirles <emily.jirles@gmail.com>
2018-04-24 14:50:34 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,11 @@
 def fizzbuzz(num)
-  if num % 3 == 0 
+  if num % 3 == 0
     if num % 5 == 0 
-      return "FizzBuzz"
+      return "FizzBuzz"  
     end
     return "Fizz"
   end
   elsif num % 5 == 0 
-    return "Buzz" 
-  end 
-  else 
-    return nil 
-  end 
+    return "Buzz"
+  end
 end
